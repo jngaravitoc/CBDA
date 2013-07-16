54f365c79a0d856d8b1e66151386c91dc179356c
jngaravitoc <nico.garavito@gmail.com>
2013-07-16 18:15:24 -0430

    Working with and external file

--- src/plots.py
+++ src/plots.py
@@ -1,34 +1,49 @@
 from matplotlib import pyplot as plt
 from math import *
 import numpy as np
+import sys
+import scipy as sc
 
+if len(sys.argv) < 2:
+        print 'please provide a script with the following information:'
+else:
+        x = sc.genfromtxt(sys.argv[1], dtype='S')
+	dic = {}
+	for i in range(len(x[:,0])):
+		(key, val) = (x[i,0], x[i, 2])
+		dic[(key)] = val
+	#ic = {}
+        #ifile =  open(sys.argv[1], 'r') 
+        
+#	for line in ifile.readline():
+#		print line
+#		(key, val) = line.split(' = ')
+ #               dic[(key)] = val
+#print di
+hist = float(dic['Histogram'])
+Residues = float(dic['Residues'])
+density_1 = np.loadtxt(dic['densities_path_1'])
+#density_2 = np.loadtxt(dic['densities_path_2'])
+data_1 = np.loadtxt(dic['data_path_1'])
+#data_2 = np.loadtxt(dic['data_path_2'])
+out_fig_path = dic['out_fig_path']
+n_bins = float(dic['n_bins'])
+histo_xlabel = dic['histo_xlabel']
+histo_ylabel = dic['histo_ylabel']
+histo_title = dic['histo_title']
+histo_xsize = float(dic['histo_xsize'])
+histo_ysize = float(dic['histo_ysize'])
+x_data_row = float(dic['x_data_row'])
+y_data_row = float(dic['y_data_row'])
 
-# Defining data paths.
+def histogram(out_fig_path, n_bins, histo_xlabel, histo_ylabel,histo_title, histo_xsize, histo_ysize, x_data_row, y_data_row):
+	x = density_1[:,0]
+	y = density_1[:,1]
+	n_0 = density_1[:,2]
+	X = data_1[:,x_data_row]/15  #Divide by 15 when expressing AR in hours
+	Y = data_1[:,y_data_row]
 
-density = np.loadtxt('../output/Layden_ARvsRhel.txt')  # This is the data comming from CBDA.py and have the density and the sigma
-density2 = np.loadtxt('../output/DensityData_SynthQuestX100_ARvsRhel.txt')
-#ata = np.loadtxt('../input/synthetic_data.dat')  # this is the Data to make the scatter plot. (Initial distribution)
-#data = np.loadtxt('/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/Quest_cut.dat')
-data = np.loadtxt("/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/layden.rrls.mateu_dists.dat")
-#data = np.loadtxt("/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/sesar.rrls.mydists.dat")
-#data = np.loadtxt("/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/JJD_BD.dat")
-#data = np.loadtxt("/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/JJD_VLMS.dat")
-data2 = np.loadtxt("/home/nicolas/Dropbox/github/Bayesian_Homeworks/CBDA/input/synth.quest.x100.dat")
-
-
-
-global Out_fig_path
-Out_fig_path  = '../output/plots/Layden_RAvsRhel.png'
-
-def histogram(Out_fig_path, Res, X_label, Y_label, Title, Fig_xsize, Fig_ysize, Xdata_row, Ydata_row):
-
-	x = density[:,0]
-	y = density[:,1]
-	n_0 = density[:,2]
-	X = data[:, Xdata_row]/15  #Divide by 15 when expressing AR in hours
-	Y = data[:, Ydata_row]
-
-	fig = plt.figure(num=None, figsize=(Fig_xsize,Fig_ysize))
+	fig = plt.figure(num=None, figsize=(histo_xsize,histo_ysize))
 	ax = fig.add_subplot(111)
 	xmin = np.amin(x)
 	xmax = np.amax(x) 
@@ -40,26 +55,25 @@ def histogram(Out_fig_path, Res, X_label, Y_label, Title, Fig_xsize, Fig_ysize, 
 	print 'ymin=', np.amin(y)
 	print 'ymax=', np.amax(y)
 
-	plt.xlabel('$\mathrm{' + str(X_label) + '}$', fontsize = 35)
-	plt.ylabel('$\mathrm{' + str(Y_label) + '}$', fontsize = 35)
-	plt.title('$\mathrm{'+ str(Title) + '}$', fontsize = 38)
+	plt.xlabel('$\mathrm{' + str(histo_xlabel) + '}$', fontsize = 35)
+	plt.ylabel('$\mathrm{' + str(histo_ylabel) + '}$', fontsize = 35)
+	plt.title('$\mathrm{'+ str(histo_title) + '}$', fontsize = 38)
 	plt.tick_params(axis='both', which='major', labelsize=18)
 	#ax.set_tick_params(axis='both', which='major', labelsize=18)
-	hist, xedges, yedges = np.histogram2d(x, y, bins=(Res, Res),range=[[xmin,xmax],[ymin,ymax]], weights = n_0)
+	hist, xedges, yedges = np.histogram2d(x, y, bins=(n_bins, n_bins),range=[[xmin,xmax],[ymin,ymax]], weights = n_0)
 	hist = hist.transpose()
 	my_extent = (xedges[0], xedges[-1], yedges[0], yedges[-1])
-	ax.imshow(np.log10(hist), extent=[xmin, xmax, ymin, ymax], interpolation = 'gaussian', origin='lower', aspect='auto')
+	ax.imshow((hist), extent=[xmin, xmax, ymin, ymax], interpolation = 'gaussian', origin='lower', aspect='auto')
 	plt.scatter(X , Y, s = 1.5)
 	#plt.xlim([20, 24])
 	#plt.xlim([0, 4])
-	plt.savefig(str(Out_fig_path))
+	plt.savefig(str(out_fig_path))
 	plt.show()
 
+if hist == 1:
+	histogram(out_fig_path, n_bins, histo_xlabel, histo_ylabel,histo_title, histo_xsize, histo_ysize, x_data_row, y_data_row)
 
-histogram(Out_fig_path, 200, 'AR', 'R_{hel}', 'Layden Catalogue Density', 9.5, 9.5,  0,  4)
-
-
-  
+ 
 
 def residuos(Out_fig_path, Res, X_label, Y_label, Title, Fig_xsize, Fig_ysize, Xdata_row, Ydata_row, Xdata2_row, Ydata2_row):
 	x = density[:,0]
@@ -103,16 +117,17 @@ def residuos(Out_fig_path, Res, X_label, Y_label, Title, Fig_xsize, Fig_ysize, X
 	plt.tick_params(axis='both', which='major', labelsize=18)
 	#ax.set_tick..
 	#ax.set_tick_params(axis='both', which='major', labelsize=18)
-	hist, xedges, yedges = np.histogram2d(x, y, bins=(Res, Res),range=[[xmin,xmax],[ymin,ymax]], weights = n_0)
+	# generalizar a mismos xmin y xmax
+	ist, xedges, yedges = np.histogram2d(x, y, bins=(Res, Res),range=[[xmin,xmax],[ymin,ymax]], weights = n_0)
 	hist = hist.transpose()
 	my_extent = (xedges[0], xedges[-1], yedges[0], yedges[-1])
-	hist2, xedges2, yedges2 = np.histogram2d(x2, y2, bins=(200, 200),range=[[xmin2,xmax2],[ymin2,ymax2]], weights = n_02)
+	hist2, xedges2, yedges2 = np.histogram2d(x2, y2, bins=(Res, Res),range=[[xmin2,xmax2],[ymin2,ymax2]], weights = n_02)
 	hist2 = hist.transpose()
 	my_extent = (xedges2[0], xedges2[-1], yedges2[0], yedges2[-1])
-	ax.imshow((hist*10/hist2), extent=[xmin2, xmax2, ymin2, ymax2], interpolation = 'gaussian', origin='lower', aspect='auto')
-	#colorbar(shrink = 0.05)    #this shrink is to set the scale of the colorbar
+	im = ax.imshow((hist*10/hist2), extent=[xmin2, xmax2, ymin2, ymax2], interpolation = 'gaussian', origin='lower', aspect='auto')
+	plt.colorbar(im, shrink = 0.05)    #this shrink is to set the scale of the colorbar
 	plt.savefig(str(Out_fig_path))
 	plt.show()
 
-
-#residuos(Out_fig_path, 200, 'AR', 'R_{hel}', 'Residuos  Quest/Questx100', 9.5, 9.5,  0,  1, 0, 4)
+if Residues == 1:
+	residuos(Out_fig_path, 200, 'AR', 'R_{hel}', 'Residuos  Quest/Questx100', 9.5, 9.5,  0,  4, 0, 4)
