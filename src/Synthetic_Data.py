df0b713faa49bacddb3c68483320e90dec169617
jngaravitoc <nico.garavito@gmail.com>
2013-08-01 10:15:55 -0500

    3d synthetic data

--- src/Synthetic_Data.py
+++ src/Synthetic_Data.py
@@ -15,7 +15,7 @@ def Init_Distribution(Dx, Dy, nx, ny):
     for i in range(nx):
         f.write(str(X[i]) + "  " + str(Y[i]) + "\n")
     f.close()
-    #figure(num=None, figsize=(9.5,9))
+   #figure(num=None, figsize=(9.5,9))
     #xlabel('$\mathrm{x}$', fontsize = 35)
     #ylabel('$\mathrm{y}$', fontsize = 35)
     #title('$\mathrm{Random}$ $\mathrm{Cartesian}$ $\mathrm{Distribution}$ ', fontsize = 40)
@@ -42,4 +42,21 @@ def Init_Distribution_Polar(nR):
     #savefig('RandomPolarDistribution.png')
     return R, theta
 
-Init_Distribution(100, 100, 10000, 10000)
+#nit_Distribution(100, 100, 10000, 10000)
+def Init_Distribution3D(Dx, Dy, Dz, nx, ny, nz):
+    f = open('../input/3Dsynthetic_data.dat', 'w')
+    global X
+    global Y
+    global Z
+    X = np.random.random(nx)*Dx
+    Y = np.random.random(ny)*Dy
+    Z = np.random.random(nz)*Dz
+    X = np.append(X, np.random.normal(loc= 0.80*Dx, scale = 2, size = 300))
+    Y = np.append(Y, np.random.normal(loc= 0.60*Dy, scale = 8, size = 300))
+    X = np.append(X, np.random.normal(loc= 0.20*Dx, scale = 5, size = 100))
+    Y = np.append(Y, np.random.normal(loc= 0.30*Dy, scale = 5, size = 100))
+    for i in range(nx):
+        f.write(str(X[i]) + "  " + str(Y[i]) + "   " + str(Z[i]) + "\n")
+    f.close()
+ 
+Init_Distribution3D(100, 100, 100, 10, 10, 10)
